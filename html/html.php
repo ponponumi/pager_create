88050eeba05f7a614938bc73b5ff651b73c4923c
ponponumi <ponponumi@gmail.com>
2024-10-30 01:50:59 +0900

    HTMLのテスト用のファイルを作成

---
+++ html/html.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Ponponumi\PagerCreate\Web;
+
+$web = new web(3,8,5);
+echo $web->htmlCreateCallback(function($id){
+    return "./web.php?id=" . $id;
+});
+echo $web->htmlCreateUrlReplace("./index.php?id={pageid}","{pageid}");
+
+$web = new web(10,30,5);
+echo $web->htmlCreateCallback(function($id){
+    return "./web.php?id=" . $id;
+});
+echo $web->htmlCreateUrlReplace("./index.php?id={pageid}","{pageid}");
