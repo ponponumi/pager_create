86e6f11ff4df8504e11799f33e63d8c4c5c9d3e7
ponponumi <ponponumi@gmail.com>
2024-10-30 00:24:42 +0900

    リストを返すメソッドを呼び出すテストを追加

--- html/index.php
+++ html/index.php
@@ -6,9 +6,13 @@ use Ponponumi\PagerCreate\Core;
 
 $core = new Core(5,6,5);
 var_dump($core->rangeCalc());
+var_dump($core->rangeListGet());
 $core = new Core(1,3,5);
 var_dump($core->rangeCalc());
+var_dump($core->rangeListGet());
 $core = new Core(8,13,5);
 var_dump($core->rangeCalc());
+var_dump($core->rangeListGet());
 $core = new Core(1,8,5);
 var_dump($core->rangeCalc());
+var_dump($core->rangeListGet());
