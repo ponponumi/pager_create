784c0ed550ab69932d00fe1aa1c0c7558c4824f9
ponponumi <ponponumi@gmail.com>
2024-10-29 23:58:38 +0900

    コンストラクタを追加

--- src/Core.php
+++ src/Core.php
@@ -7,4 +7,16 @@ class Core{
     public int $now;
     public int $display;
     public string|null $ellipsis = "…";
+
+    public function __construct(int $now, int $max = 1, int $display = 5)
+    {
+        if($max < $now){
+            $this->now = $max;
+        }else{
+            $this->now = $now;
+        }
+
+        $this->max = $max;
+        $this->display = $display;
+    }
 }
