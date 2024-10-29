bca0ece10a486b5d4783b88f054d8f09dd3e6d5c
ponponumi <ponponumi@gmail.com>
2024-10-30 01:18:01 +0900

    テストコードを追加

--- html/web.php
+++ html/web.php
@@ -3,3 +3,8 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use Ponponumi\PagerCreate\Web;
+
+$web = new web(3,8,5);
+var_dump($web->dataCreate(function($id){
+    return "./web.php?id=" . $id;
+}));
