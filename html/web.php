b3a24319a1327aa449ac6836e6df1773328a2080
ponponumi <ponponumi@gmail.com>
2024-10-30 01:34:51 +0900

    テストを追加(nullになる問題が発生)

--- html/web.php
+++ html/web.php
@@ -8,8 +8,10 @@ $web = new web(3,8,5);
 var_dump($web->dataCreate(function($id){
     return "./web.php?id=" . $id;
 }));
+var_dump($web->dataCreateUrlReplace("./index.php?id={pageid}","{pageid}"));
 
 $web = new web(10,30,5);
 var_dump($web->dataCreate(function($id){
     return "./web.php?id=" . $id;
 }));
+var_dump($web->dataCreateUrlReplace("./index.php?id={pageid}","{pageid}"));
