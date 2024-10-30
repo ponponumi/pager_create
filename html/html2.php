3b0a01810d6e6cbf26ce1d0cf740ac9725ae0973
ponponumi <ponponumi@gmail.com>
2024-10-30 14:12:12 +0900

    テストを追加

--- html/html2.php
+++ html/html2.php
@@ -7,4 +7,7 @@ use Ponponumi\PagerCreate\Web;
 $web = new web(3,8,5);
 echo $web->htmlCreateUrlReplace("./index.php?id={pageid}","{pageid}",[
     "ulAttribute" => "#pager.pagerList.menu",
+    "liAttribute" => "#item.item",
+    "nowAttribute" => "#now.item.now",
+    "ellipsisAttribute" => "#ellipsis.item.ellipsis",
 ]);
