aefcab2c8aafcd51f004784cd963e2f6b197f65b
ponponumi <ponponumi@gmail.com>
2024-10-31 18:06:58 +0900

    試しに小さな値を渡すように変更

--- html/html3.php
+++ html/html3.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 use Ponponumi\PagerCreate\Web;
 
-$web = new web(1,1,5);
+$web = new web(-3,0,5);
 echo $web->htmlCreateUrlReplace("./index.php?id={pageid}","{pageid}",[
     "ulAttribute" => "#pager.pagerList.menu",
     "liAttribute" => "#item.item",
