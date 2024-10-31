6d7a14d707c1bee43b634f15d1cb05a66e5c8053
ponponumi <ponponumi@gmail.com>
2024-10-31 17:57:24 +0900

    新たなテストファイルを作成

---
+++ html/html3.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Ponponumi\PagerCreate\Web;
+
+$web = new web(1,1,5);
+echo $web->htmlCreateUrlReplace("./index.php?id={pageid}","{pageid}",[
+    "ulAttribute" => "#pager.pagerList.menu",
+    "liAttribute" => "#item.item",
+    "nowAttribute" => "#now.item.now",
+    "ellipsisAttribute" => "#ellipsis.item.ellipsis",
+    "aroundButtonMode" => true,
+    "prevAttribute" => "#prev-btn",
+    "nextAttribute" => "#next-btn",
+    "nowNotLink" => true,
+]);
