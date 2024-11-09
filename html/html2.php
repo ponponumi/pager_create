5c5b2a16111f427c932746493fc5e9da7d126c8a
ponponumi <ponponumi@gmail.com>
2024-11-09 18:12:39 +0900

    最初と最後のボタンを出力しないテストを追加(成功)

--- html/html2.php
+++ html/html2.php
@@ -5,6 +5,7 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use Ponponumi\PagerCreate\Web;
 
 $web = new web(7,13,5);
+$web->firstAndLastModeChange(false);
 echo $web->htmlCreateUrlReplace("./index.php?id={pageid}","{pageid}",[
     "ulAttribute" => "#pager.pagerList.menu",
     "liAttribute" => "#item.item",
