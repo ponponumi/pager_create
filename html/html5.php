f71d5ce2ef63354631869c0a2bf7234a885e9684
ponponumi <ponponumi@gmail.com>
2024-12-11 17:03:29 +0900

    テスト用のファイルを追加

---
+++ html/html5.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Ponponumi\PagerCreate\Web;
+
+$web = new web($_GET["id"] ?? 1, 30, 5);
+$web->idAndClassDirectly(true);
+echo $web->htmlCreateUrlReplace("./html5.php?id={pageid}","{pageid}",[
+    "ulAttribute" => 'id="pager" class="pagerList menu"',
+    "liAttribute" => ' class="item"',
+    "nowAttribute" => 'id="now" class="item now"',
+    "ellipsisAttribute" => 'class="item ellipsis"',
+    "prevAttribute" => 'id="prev-btn"',
+    "nextAttribute" => 'id="next-btn"',
+]);
